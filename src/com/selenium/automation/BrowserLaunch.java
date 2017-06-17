34d492a2d27c7e016ce62043ddbc7f8dd95aa565
sys <kumarabhi598@gmail.com>
2017-06-17 22:15:26 +0530

    Testcase updated
    Change-Id: Icd1a9bf1c1fabeb89c0f97a692a00c9528b24459
    Signed-off-by: sys <kumarabhi598@gmail.com>

---
+++ src/com/selenium/automation/BrowserLaunch.java
@@ -0,0 +1,22 @@
+package com.selenium.automation;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class BrowserLaunch {
+	public static String url="http://www.google.com";
+	public static void main(String[] args) throws MalformedURLException{
+		//DesiredCapabilities capability = DesiredCapabilities.firefox();
+		WebDriver driver;
+		//driver = new RemoteWebDriver(new URL("http://127.0.0.1:4444/wd/hub"),capability);
+		driver = new FirefoxDriver();
+		driver.get(url);
+		driver.getCurrentUrl();
+		driver.close();
+	}
+}
